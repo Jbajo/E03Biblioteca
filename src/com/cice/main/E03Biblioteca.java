86bb085b26dc298f35536e35ad13f978ed70be07
jbajo <javier.bajochacon@gmail.com>
2018-02-03 19:09:30 +0100

    Modificado ListaPrestados

--- src/com/cice/main/E03Biblioteca.java
+++ src/com/cice/main/E03Biblioteca.java
@@ -4,6 +4,10 @@ import com.cice.negocio.Biblioteca;
 
 import java.text.ParseException;
 
+/**
+ * @author Javier Bajo Chacon  javier.bajochacon@gmail.com
+ */
+
 public class E03Biblioteca {
 
     public static void main(String[] args) throws ParseException {
